52ec184984cac2ea0869ec22a574404f5bd8fed9
tak <njirtak@gmail.com>
2019-01-11 13:39:05 +0900

    feat: add routing and default components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Navigation from '../Navigation';
+import LandingPage from '../Landing';
+import HomePage from '../Home';
+import AuthPage from '../Auth';
+
+import * as ROUTES from '../../constants/routes';
 
 const App = () => (
-    <div>
-        <h1>App</h1>
-    </div>
+    <Router>
+        <div>
+            { /* TODO: add Header */}
+            <Navigation />
+            <hr />
+
+            <Route exact path={ROUTES.LANDING} component={LandingPage} />
+            <Route path={ROUTES.AUTH} component={AuthPage} />
+            <Route path={ROUTES.HOME} component={HomePage} />
+        </div>
+
+    </Router>
 );
 
 export default App;
---
+++ src/components/Auth/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AuthPage = () => (
+    <div>
+        Auth Page
+    </div>
+);
+
+export default AuthPage;
