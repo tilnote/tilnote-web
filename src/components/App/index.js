48e7a96ac4c2d63df53008972ec61258407db7ee
tak <njirtak@gmail.com>
2019-01-25 16:15:41 +0900

    feat: add withRouter to SubHeader

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Header from '../Header';
 import SubHeader from '../SubHeader';
-import Navigation from '../Navigation';
 import LandingPage from '../Landing';
 import HomePage from '../Home';
 import SignInPage from '../SignIn';
@@ -17,9 +16,6 @@ const App = () => (
         <div style={{height: '100%', background: 'rgb(240, 240, 240)'}}>
             <Header />
             <SubHeader />
-            {/* TODO: delete below Navigation */}
-            {/* <Navigation /> */}
-
             <Route exact path={ROUTES.LANDING} component={LandingPage} />
             <Route path={ROUTES.HOME} component={HomePage} />
             <Route path={ROUTES.SIGN_IN} component={SignInPage} />
